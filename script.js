6445eefb85fd99693496bfb2f3334e1352b8adc0
Aaron <aaron.yepez23@gmail.com>
2026-05-11 12:55:18 -0500

    feat: avance lab 3

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log('¡Hola desde JavaScript!-Aaron Yepez');
+
+
+let nombrePropio = 'Aaron';
+
+
+let apellidoPaterno ;
+
+apellidoPaterno= 'Yepez';
+
+const CURSO= "Code 101"
+
+let saludo1 = "Hola" + nombrePropio + " "+ apellidoPaterno + "y estoy en el curso" + CURSO
+
+let saludoModerno= "hola me llamo ${nombrePropio} ${apellidoPaterno} y estoy en el curso ${CURSO}"
+
+console.log(saludo1);
+console.log(saludoModerno);
+
+let a = 20;
+let b = 7;
+
+console.log( a + b);
+
+console.log( a - b);
+
+let edadUsuario = prompt("¿cuantos años tienes?")
+ 
+console.log(edadUsuario)
+console.log(typeof edadUsuario);
+
+edadUsuario=Number (edadUsuario)
+
+console.log(typeof edadUsuario);
+console.log(edadUsuario+4);
+
+
+
+console.log(CURSO)
+
+console.log(typeof nombrePropio);
+console.log(typeof CURSO);
+
+console.log("42");
+console.log(42)
